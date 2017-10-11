23c08bbe631dd8004e00b8a87ed907c6ea6e8949
vamsi krishna reddy kamjula <vamsikvk916@gmail.com>
2017-10-11 14:15:14 -0400

    Initial Commit

---
+++ YTApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YTApplication
+//
+//  Created by vamsi krishna reddy kamjula on 10/11/17.
+//  Copyright © 2017 vamsikvkrApplicationDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
